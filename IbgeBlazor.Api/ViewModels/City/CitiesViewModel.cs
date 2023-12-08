d7ecae3c5df7fe14f03261cdb570a03826b42ddf
Alex Marafon <alex-asam@hotmail.com>
2023-12-08 16:43:03 -0400

    Feat: Adicionado classe para Cidade

---
+++ IbgeBlazor.Api/ViewModels/City/CitiesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IbgeBlazor.Api.ViewModels.City
+{
+    public class CitiesViewModel
+    {
+        public string Id { get; set; }
+        public string CodeCitie { get; set; }
+        public string City { get; set; }
+        public string Uf { get; set; }
+        public string State { get; set; }
+    }
+}
