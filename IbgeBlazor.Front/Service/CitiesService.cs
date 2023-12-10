de9c058f4504a037ad11dd6e406a47ea3c752263
thiago zermiani <thiago.andre@leankeep.com.br>
2023-12-10 10:32:56 -0300

    feat: adicao da tela cities

---
+++ IbgeBlazor.Front/Service/CitiesService.cs
@@ -0,0 +1,43 @@
+﻿using IbgeBlazor.Front.Model;
+using Mono.TextTemplating;
+
+namespace IbgeBlazor.Front.Service
+{
+    public class CitiesService
+    {
+        public CityModel GetCity(string id)
+        {
+            return new CityModel()
+            {
+                Id = id,
+                City = "Curitiba",
+                Uf = "PR",
+                State = "Paraná",
+            };
+
+        }
+        public List<CityModel> GetCityList()
+        {
+            return new List<CityModel>() {
+                new CityModel() {
+                    Id = "1",
+                    City = "Curitiba",
+                    Uf = "PR",
+                    State = "Paraná",
+                },
+                new CityModel() {
+                    Id = "2",
+                    City = "São Paulo",
+                    Uf = "SP",
+                    State = "São Paulo",
+                },
+                new CityModel() {
+                    Id = "3",
+                    City = "Rio de Janeiro",
+                    Uf = "RJ",
+                    State = "Rio de Janeiro",
+                },
+            };
+        }
+    }
+}
